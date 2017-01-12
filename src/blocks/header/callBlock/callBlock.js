07e00861d9bcb0ffb8dd79b2610d94dc989eb4e2
Mercury Rory <Mercury Rory>
2017-01-12 17:15:55 +0300

    A half with an hour of a work

--- src/blocks/header/callBlock/callBlock.js
+++ src/blocks/header/callBlock/callBlock.js
@@ -1,23 +1,18 @@
-var clientW = 0;
-$(window).resize(function() {
-  var clientW = window.innerHeight;
-});
 var $callIcon = $('.call__icon i');
 var $tel = $('.call__tel');
+
 $callIcon.hover(
   function() {
     $callIcon
       .removeClass('fa-phone')
       .addClass('fa-volume-control-phone');
-    $tel.stop().fadeIn();
   },
   function() {
     $callIcon
       .removeClass('fa-volume-control-phone')
-      .addClass('fa-phone');
-    if (clientW < 1199 && clientW > 992) {
-      $tel.stop().fadeOut("slow");
-    }
+      .addClass('fa-phone');     
   }                    
 );// end hover
+
+
 
