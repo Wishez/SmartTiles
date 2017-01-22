8582f74d329a46a0a6d5d958bae4e0154b7959d5
Mercury Rory <Mercury Rory>
2017-01-22 09:36:15 +0300

    The respinsive firm block is in the process making.

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'breakpoint'
 http_path = "/"
 css_dir = "build/css"
 sass_dir = "src/scss"
-images_dir = "src/img"
+images_dir = "build/img"
 javascripts_dir = "src/js"
 fonts_dir = "src/fonts"
 
