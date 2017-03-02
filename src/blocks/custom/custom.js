5bd6a1a5cd009acfc55802c9f048bfd51e63c698
Wishez <wishez@list.ru>
2017-03-02 16:01:13 +0300

    Added modernizr and more cross-browser.

--- src/blocks/custom/custom.js
+++ src/blocks/custom/custom.js
@@ -144,3 +144,10 @@ var insertProperty = function(string, propName, propValue) {
   
   return string;
 };
+
+// For ie10 styles.
+var b = document.documentElement;
+b.setAttribute('data-useragent',  navigator.userAgent);
+b.setAttribute('data-platform', navigator.platform );
+
+// IE 10 == Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)
