b4c8f1b933c02aeecb779a66d03d70124f3bd595
Wishez <wishez@list.ru>
2017-03-02 22:20:36 +0300

    More cross-browser.

--- src/blocks/custom/custom.js
+++ src/blocks/custom/custom.js
@@ -146,8 +146,8 @@ var insertProperty = function(string, propName, propValue) {
 };
 
 // For ie10 styles.
-var b = document.documentElement;
-b.setAttribute('data-useragent',  navigator.userAgent);
-b.setAttribute('data-platform', navigator.platform );
-
-// IE 10 == Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)
+//var b = document.documentElement;
+//b.setAttribute('data-useragent',  navigator.userAgent);
+//b.setAttribute('data-platform', navigator.platform );
+//
+//// IE 10 == Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)
